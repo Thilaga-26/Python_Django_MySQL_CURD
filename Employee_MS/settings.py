95bb5769b1eb7ee7c7fa9e10c810b0560de0ac8a
Thilaga-26 <112231561+Thilaga-26@users.noreply.github.com>
2024-04-15 13:46:20 +0530

    first commit

--- Employee_MS/settings.py
+++ Employee_MS/settings.py
@@ -21,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+# Load variables from .env file
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=7a^@w$jat8)5o%r%3jrm%ua+b5#seteg3v_knplvjahm!ohl'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
